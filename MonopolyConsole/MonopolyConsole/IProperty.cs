9c7c5e39c8bb331d042194448fc117bc21204de4
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2017-10-26 00:17:23 -0400

    Add files via upload

--- MonopolyConsole/MonopolyConsole/IProperty.cs
+++ MonopolyConsole/MonopolyConsole/IProperty.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace MonopolyConsole
 {
-    interface IProperty
+    interface IProperty 
     {
         Player Owner { get; set; }       // player who owns the tile
-        String OwnerName { get; set; }   // name of the player who owns the tile
         String Color { get; set; }       // color of the tile
         String Name { get; set; }        // name of the tile
         int Rent { get; set; }           // rent cost of tile
         int NumOfHouses { get; set; }    // number of houses currently on the tile
         int CostOfHouse { get; set; }    // cost of a house
         int CostOfHotel { get; set; }    // cost of a hotel
+        int HouseValue { get; set; }     // how much a house is worth sold
+        int HotelValue { get; set; }     // how much a hotel is worth sold
         int MortgageValue { get; set; }  // mortgage value
         int MortgageCost { get; set; }   // cost to unmortgage
         int PropertyCost { get; set; }   // cost of the property
@@ -24,11 +25,11 @@ namespace MonopolyConsole
         bool IsOwned { get; set; }       // is the tile owned by any player
 
         void TileAction(Player player);
+        bool Equals(Property property);
         int RentCost();
+        bool Mortgage();  
+        bool Unmortgage();
         void BuildHouse();
         void BuildHotel();
-        bool Mortgage();
-        bool Unmortgage();
-
     }
 }
--- MonopolyConsole/MonopolyConsole/Program.cs
+++ MonopolyConsole/MonopolyConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MonopolyConsole.Tiles;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,39 @@ namespace MonopolyConsole
     {
         static void Main(string[] args)
         {
+            IProperty boardwalk = new Boardwalk();
+            IProperty parkplace = new ParkPlace();
 
+            Player player = new Player("George Clooney");
+
+            Console.WriteLine($"PLAYER {player.PlayerName} has ${player.GetBalance()}. Change his name now!");
+
+            player.AddProperty(boardwalk);
+            player.AddProperty(parkplace);
+
+            Console.WriteLine($"PLAYER {player.PlayerName} owns {player.GetProperty(player.GetIndexOfProperty(boardwalk)).Name} and " +
+                $"{player.GetProperty(player.GetIndexOfProperty(parkplace)).Name}. Change his name now!");
+
+            player.RemoveProperty(parkplace);
+            player.RemoveProperty(parkplace);
+            player.RemoveProperty(parkplace);
+
+            player.AddProperty(boardwalk);
+            player.AddProperty(boardwalk);
+
+            player.RemoveProperty(boardwalk);
+
+            player.AddProperty(parkplace);
+            player.AddProperty(boardwalk);
+
+            player.RemoveProperty(parkplace);
+            player.RemoveProperty(boardwalk);
+
+            String input = Console.ReadLine();
+
+            player.PlayerName = input;
+            
+            Console.WriteLine($"PLAYER {player.PlayerName}");
         }
     }
 }
